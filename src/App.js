55df3b1e9e6066085aaa41815782d5487ea23c89
anjesh-bit <anjesh67890@gmail.com>
2022-11-23 15:39:54 +0545

    folder structures

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import NavbarPage from './pages/NavbarPage';
+function App() {
+  return (
+    <div className="App">
+      <NavbarPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/NavbarPage.jsx
@@ -0,0 +1,9 @@
+import Navbar from "../components/Navbar";
+const NavbarPage = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
+export default NavbarPage;
